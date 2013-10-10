d5632cc0da649bf16bac8a068f827d05d989f34b
Peter WONG Wing Lun <luiges90@gmail.com>
2013-10-10 12:11:43 +0800

    disqus script

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -52,5 +52,19 @@ $(function(){
 			}
 		});
 	})();
+	
+	(function(){
+		/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+		var disqus_shortname = 'luiges90'; // required: replace example with your forum shortname
+		var disqus_identifier = $('h1').text().replace(/\W/gi, '-') || 'id';
+		var disqus_title = $('h1').text() || '';
+
+		/* * * DON'T EDIT BELOW THIS LINE * * */
+		(function() {
+			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+		})();
+	})();
 });
 
